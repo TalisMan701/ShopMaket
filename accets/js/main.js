6fc46aa2e7b30aaa0b5deb11fc7bf23cd66473e7
TalisMan701 <boss.kompaneec@mail.ru>
2019-08-10 16:19:19 +0500

    Catalog

--- accets/js/main.js
+++ accets/js/main.js
@@ -1,4 +1,58 @@
 $(document).ready(function(){
+
+  /* catalog */
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 75, 300 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val(ui.values[ 0 ]);
+      $( "#amount-1" ).val(ui.values[ 1 ]);
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ));
+  $( "#amount-1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ));
+
+  // Изменение местоположения ползунка при вводиде данных в первый элемент input
+  $("input#amount").change(function(){
+    var value1=$("input#amount").val();
+    var value2=$("input#amount_1").val();
+      if(parseInt(value1) > parseInt(value2)){
+      value1 = value2;
+      $("input#amount").val("$"+value1);
+    }
+    $("#slider-range").slider("values",0,value1);	
+  });
+      
+  // Изменение местоположения ползунка при вводиде данных в второй элемент input 	
+  $("input#amount-1").change(function(){
+    var value1=$("input#amount").val();
+    var value2=$("input#amount-1").val();
+
+    if(parseInt(value1) > parseInt(value2)){
+      value2 = value1;
+      $("input#amount-1").val(value2);
+    }
+    $("#slider-range").slider("values",1,value2);
+  });
+
+  // фильтрация ввода в поля
+  jQuery('#amount, #amount-1').keypress(function(event){
+    var key, keyChar;
+    if(!event) var event = window.event;
+    
+    if (event.keyCode) key = event.keyCode;
+    else if(event.which) key = event.which;
+  
+    if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+    keyChar=String.fromCharCode(key);
+    
+    if(!/\d/.test(keyChar))	return false;
+  
+  });
+
   let comingdate = new Date("Aug 1, 2019 08:02:05");
 
   let d = document.getElementById('d');
@@ -113,4 +167,6 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="btn slick-prev"><svg><use xlink:href="#arrow"></use></svg></button>',
     nextArrow: '<button type="button" class="btn slick-next"><svg><use xlink:href="#arrow"></use></svg></button>'
   });
+
+  
 });
